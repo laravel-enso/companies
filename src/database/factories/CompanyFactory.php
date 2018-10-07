44a409c6a3a50734a87a7cbbb0e6e71ddb974c5e
mishu <mishu@xtelecom.ro>
2018-10-07 18:36:47 +0300

    fixes factory imports

--- src/database/factories/CompanyFactory.php
+++ src/database/factories/CompanyFactory.php
@@ -1,19 +1,21 @@
 <?php
 
 use Faker\Generator as Faker;
+use LaravelEnso\People\app\Models\Person;
+use LaravelEnso\Companies\app\Models\Company;
 
 $factory->define(Company::class, function (Faker $faker) {
     return [
         'mandatary_id' => function () {
-            return factory(Company::class)->create()->id;
+            return factory(Person::class)->create()->id;
         },
         'name' => $faker->company,
         'email' => $faker->email,
         'phone' => $faker->phoneNumber,
-        'fax' => $faker->faxNumber,
+        'fax' => $faker->phoneNumber,
         'mandatary_position' => $faker->jobTitle,
         'bank' => $faker->company,
-        'account' => $faker->bank_account,
+        'bank_account' => $faker->bankAccountNumber,
         'obs' => $faker->sentence,
         'pays_vat' => $faker->boolean,
     ];
