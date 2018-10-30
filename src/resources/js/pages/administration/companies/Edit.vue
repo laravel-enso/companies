94cf315fcf1c2685bd1a5d638aa18eb1e7cd5f8d
mishu <mishu@xtelecom.ro>
2018-10-30 10:18:57 +0200

    fixes accessories use

--- src/resources/js/pages/administration/companies/Edit.vue
+++ src/resources/js/pages/administration/companies/Edit.vue
@@ -9,10 +9,19 @@
                 <accessories type="LaravelEnso\Companies\app\Models\Company"
                     :id="$refs.form.data.routeParams.company"
                     addresses
-                    contacts
-                    comments
                     discussions
-                    documents/>
+                    documents>
+                    <template slot="custom"
+                        slot-scope="{count}">
+                        <tab keep-alive
+                             id="Contacts">
+                            <contacts controls
+                                :id="$refs.form.data.routeParams.company"
+                                @update="$set(count, 'contacts', $refs.contacts.count)"
+                                ref="contacts"/>
+                        </tab>
+                    </template>
+                </accessories>
             </div>
         </div>
     </div>
@@ -23,15 +32,17 @@
 
 import Accessories from '../../../components/enso/bulma/Accessories.vue';
 import VueFormSs from '../../../components/enso/vueforms/VueFormSs.vue';
+import Contacts from '../../../components/enso/contacts/Contacts.vue';
+import Tab from '../../../components/enso/bulma/Tab.vue';
 
 export default {
-    components: { VueFormSs, Accessories },
-
-    data() {
-        return {
-            initialised: false,
-        };
+    components: {
+        VueFormSs, Accessories, Contacts, Tab,
     },
+
+    data: () => ({
+        initialised: false,
+    }),
 };
 
 </script>
