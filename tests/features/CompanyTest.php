7bc5757cdc48c7ddf346042c027b6730841c61c9
y0net <ionutpirvulescu1@gmail.com>
2018-11-05 19:05:40 +0200

    adds phpunit tests

---
+++ tests/features/CompanyTest.php
@@ -0,0 +1,89 @@
+<?php
+
+use Faker\Factory;
+use Tests\TestCase;
+use LaravelEnso\Core\app\Models\User;
+use LaravelEnso\Companies\app\Models\Company;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use LaravelEnso\FormBuilder\app\TestTraits\EditForm;
+use LaravelEnso\FormBuilder\app\TestTraits\DestroyForm;
+use LaravelEnso\VueDatatable\app\Traits\Tests\Datatable;
+
+class CompanyTest extends TestCase
+{
+    use Datatable, DestroyForm, EditForm, RefreshDatabase;
+
+    private $permissionGroup = 'administration.companies';
+    private $testModel;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // $this->withoutExceptionHandling();
+
+        $this->seed()
+            ->actingAs(User::first());
+
+        $this->testModel = factory(Company::class)
+            ->make();
+    }
+
+    /** @test */
+    public function can_view_create_form()
+    {
+        $this->get(route($this->permissionGroup.'.create', false))
+            ->assertStatus(200)
+            ->assertJsonStructure(['form']);
+    }
+
+    /** @test */
+    public function can_store_company()
+    {
+        $response = $this->post(
+            route('administration.companies.store', [], false),
+            $this->testModel->toArray()
+        );
+
+        $company = Company::whereName($this->testModel->name)
+            ->first();
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message'])
+            ->assertJsonFragment([
+                'redirect' => 'administration.companies.edit',
+                'param' => ['company' => $company->id],
+            ]);
+    }
+
+    /** @test */
+    public function can_update_company()
+    {
+        $this->testModel->save();
+
+        $this->testModel->name = 'updated';
+
+        $this->patch(
+            route('administration.companies.update', $this->testModel->id, false),
+            $this->testModel->toArray()
+        )->assertStatus(200)
+        ->assertJsonStructure(['message']);
+
+        $this->assertEquals('updated', $this->testModel->fresh()->name);
+    }
+
+    /** @test */
+    public function get_option_list()
+    {
+        $this->testModel->save();
+
+        $this->get(route('administration.companies.options', [
+            'query' => $this->testModel->name,
+            'limit' => 10,
+        ], false))
+        ->assertStatus(200)
+        ->assertJsonFragment([
+            'name' => $this->testModel->name,
+        ]);
+    }
+}
