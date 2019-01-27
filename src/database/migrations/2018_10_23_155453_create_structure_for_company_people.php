f41eea56f13c54c939302df19d4217e6da8726e9
mishu <mishu@xtelecom.ro>
2019-01-27 10:05:48 +0200

    adds permissions check for people management

--- src/database/migrations/2018_10_23_155453_create_structure_for_company_people.php
+++ src/database/migrations/2018_10_23_155453_create_structure_for_company_people.php
@@ -6,7 +6,6 @@ class CreateStructureForCompanyPeople extends StructureMigration
 {
     protected $permissions = [
         ['name' => 'administration.companies.people.create', 'description' => 'Add person to company', 'type' => 1, 'is_default' => false],
-        ['name' => 'administration.companies.people.edit', 'description' => 'Edit existing company person', 'type' => 1, 'is_default' => false],
         ['name' => 'administration.companies.people.index', 'description' => 'Show company people', 'type' => 0, 'is_default' => false],
         ['name' => 'administration.companies.people.store', 'description' => 'Save newly added company person', 'type' => 1, 'is_default' => false],
         ['name' => 'administration.companies.people.update', 'description' => 'Update edited company person', 'type' => 1, 'is_default' => false],
