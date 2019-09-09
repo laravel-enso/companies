64840f4c8a676c18458cbb343de8aa50655bd944
mishu <mishu@xtelecom.ro>
2019-09-09 10:14:36 +0300

    removes options controller restrictions

--- src/app/Http/Controllers/Company/Options.php
+++ src/app/Http/Controllers/Company/Options.php
@@ -2,7 +2,6 @@
 
 namespace LaravelEnso\Companies\app\Http\Controllers\Company;
 
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use LaravelEnso\Companies\app\Models\Company;
 use LaravelEnso\Select\app\Traits\OptionsBuilder;
@@ -11,21 +10,5 @@ class Options extends Controller
 {
     use OptionsBuilder;
 
-    private $company;
-
-    public function __construct(Company $company)
-    {
-        $this->company = $company;
-    }
-
-    public function query(Request $request)
-    {
-        return $this->company::when(
-            ! $request->user()->belongsToAdminGroup(),
-            function ($query) use ($request) {
-                $query->whereIn(
-                    'id', $request->user()->person->companies()->pluck('id')
-                );
-            });
-    }
+    protected $model = Company::class;
 }
