7a2ce9afa1fec45e35a5bd5d79506add2b477701
mishu <mishu@xtelecom.ro>
2019-09-18 08:50:56 +0300

    visual code refactor

--- src/app/Exceptions/CompanyException.php
+++ src/app/Exceptions/CompanyException.php
@@ -8,6 +8,8 @@ class CompanyException extends EnsoException
 {
     public static function dissociateMandatary()
     {
-        return new static(__('You cannot dissociate the mandatary unless is the only one attached on this company'));
+        return new static(__(
+            'You cannot dissociate the mandatary unless is the only one attached on this company'
+        ));
     }
 }
