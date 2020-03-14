dfeaeb725860240acfb3d72f30b4ac8f185ad852
mishu <mishu@xtelecom.ro>
2020-03-14 18:09:07 +0200

    removes Types from migrations

--- src/database/migrations/2018_10_07_102000_create_structure_for_company_people.php
+++ src/database/migrations/2018_10_07_102000_create_structure_for_company_people.php
@@ -1,16 +1,15 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForCompanyPeople extends Migration
 {
     protected $permissions = [
-        ['name' => 'administration.companies.people.create', 'description' => 'Add person to company', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.companies.people.edit', 'description' => 'Edit existing company person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.companies.people.index', 'description' => 'Show company people', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'administration.companies.people.store', 'description' => 'Save newly added company person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.companies.people.update', 'description' => 'Update edited company person', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'administration.companies.people.destroy', 'description' => 'Delete company person', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'administration.companies.people.create', 'description' => 'Add person to company', 'is_default' => false],
+        ['name' => 'administration.companies.people.edit', 'description' => 'Edit existing company person', 'is_default' => false],
+        ['name' => 'administration.companies.people.index', 'description' => 'Show company people', 'is_default' => false],
+        ['name' => 'administration.companies.people.store', 'description' => 'Save newly added company person', 'is_default' => false],
+        ['name' => 'administration.companies.people.update', 'description' => 'Update edited company person', 'is_default' => false],
+        ['name' => 'administration.companies.people.destroy', 'description' => 'Delete company person', 'is_default' => false],
     ];
 }
