6282f108a1b9e306f3c66d5a5d48971875505c40
mishu <mishu@xtelecom.ro>
2020-06-25 16:17:36 +0300

    fixes helpers services namespace

--- src/Imports/Importers/Company.php
+++ src/Imports/Importers/Company.php
@@ -5,7 +5,7 @@ namespace LaravelEnso\Companies\Imports\Importers;
 use LaravelEnso\Companies\Models\Company as Model;
 use LaravelEnso\Core\Models\User;
 use LaravelEnso\DataImport\Contracts\Importable;
-use LaravelEnso\Helpers\Classes\Obj;
+use LaravelEnso\Helpers\Services\Obj;
 
 class Company implements Importable
 {
