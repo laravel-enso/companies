10d3ff596147be65a8fea5dc5b64ea75bc9a012c
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 15:44:45 +0300

    updates syntax

--- routes/app/people.php
+++ routes/app/people.php
@@ -1,17 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Companies\Http\Controllers\Person\Create;
+use LaravelEnso\Companies\Http\Controllers\Person\Destroy;
+use LaravelEnso\Companies\Http\Controllers\Person\Edit;
+use LaravelEnso\Companies\Http\Controllers\Person\Index;
+use LaravelEnso\Companies\Http\Controllers\Person\Store;
+use LaravelEnso\Companies\Http\Controllers\Person\Update;
 
-Route::namespace('Person')
+Route::prefix('people')
+    ->as('people.')
     ->group(function () {
-        Route::prefix('people')
-            ->as('people.')
-            ->group(function () {
-                Route::get('{company}', 'Index')->name('index');
-                Route::get('{company}/create', 'Create')->name('create');
-                Route::get('{company}/{person}/edit', 'Edit')->name('edit');
-                Route::patch('{person}', 'Update')->name('update');
-                Route::post('', 'Store')->name('store');
-                Route::delete('{company}/{person}', 'Destroy')->name('destroy');
-            });
+        Route::get('{company}', Index::class)->name('index');
+        Route::get('{company}/create', Create::class)->name('create');
+        Route::get('{company}/{person}/edit', Edit::class)->name('edit');
+        Route::patch('{person}', Update::class)->name('update');
+        Route::post('', Store::class)->name('store');
+        Route::delete('{company}/{person}', Destroy::class)->name('destroy');
     });
