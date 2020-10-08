66ef36b9fc52bf95626d0374fa13eb4edd21b091
Mihai <mihai.ocneanu@gmail.com>
2020-10-08 11:09:33 +0300

    fixed factory namespace

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LaravelEnso\Core\Database\Factories;
+namespace LaravelEnso\Companies\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use LaravelEnso\Companies\Enums\Statuses;
