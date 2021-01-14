da5426760c599d9c417e3c8feb6c14fdec864d97
reza <raftx24@gmail.com>
2021-01-14 22:35:19 +0330

    adds missing trait

--- src/Http/Controllers/Company/Edit.php
+++ src/Http/Controllers/Company/Edit.php
@@ -2,12 +2,15 @@
 
 namespace LaravelEnso\Companies\Http\Controllers\Company;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller;
 use LaravelEnso\Companies\Forms\Builders\CompanyForm;
 use LaravelEnso\Companies\Models\Company;
 
 class Edit extends Controller
 {
+    use AuthorizesRequests;
+
     public function __invoke(Company $company, CompanyForm $form)
     {
         $this->authorize('update', $company);
