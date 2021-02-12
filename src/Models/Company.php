2b5e7ec59f96ae4c36e94d27e7e97ec1e3a654fa
Adrian Ocneanu <aocneanu@users.noreply.github.com>
2021-02-12 13:24:43 +0000

    Apply fixes from StyleCI

--- src/Models/Company.php
+++ src/Models/Company.php
@@ -35,7 +35,7 @@ class Company extends Model
     protected $casts = ['pays_vat' => 'boolean', 'is_tenant' => 'boolean'];
 
     protected array $rememberableKeys = ['id', 'name'];
-    
+
     public function people()
     {
         return $this->belongsToMany(Person::class)
